79828dc116a86da08f8494ff5c446fc8729a00e5
usuario <usuario@pc20-002>
2023-10-10 10:17:07 +0200

    TDD 1.1

--- src/pkg/Calculadora.java
+++ src/pkg/Calculadora.java
@@ -2,5 +2,15 @@ package pkg;
 
 public class Calculadora {
 
+	public static Object suma(int i, int j) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static Object resta(int i, int j) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 		
 }
